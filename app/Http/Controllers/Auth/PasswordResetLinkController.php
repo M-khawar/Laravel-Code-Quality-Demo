74c935d0b02b7e69379663336743d902cd3eb506
M-khawar <muhammad.khawar@ilsainteractive.com>
2023-07-10 16:41:32 +0500

    [bug] reset-password & forgot-password error response corrected

--- app/Http/Controllers/Auth/PasswordResetLinkController.php
+++ app/Http/Controllers/Auth/PasswordResetLinkController.php
@@ -17,23 +17,28 @@ class PasswordResetLinkController extends Controller
      */
     public function store(Request $request): JsonResponse
     {
-        $request->validate([
-            'email' => ['required', 'email'],
-        ]);
-
-        // We will send the password reset link to this user. Once we have attempted
-        // to send the link, we will examine the response then see the message we
-        // need to show to the user. Finally, we'll send out a proper response.
-        $status = Password::sendResetLink(
-            $request->only('email')
-        );
-
-        if ($status != Password::RESET_LINK_SENT) {
-            throw ValidationException::withMessages([
-                'email' => [__($status)],
+        try {
+            $request->validate([
+                'email' => ['required', 'email'],
             ]);
-        }
 
-        return response()->message(__($status));
+            // We will send the password reset link to this user. Once we have attempted
+            // to send the link, we will examine the response then see the message we
+            // need to show to the user. Finally, we'll send out a proper response.
+            $status = Password::sendResetLink(
+                $request->only('email')
+            );
+
+            if ($status != Password::RESET_LINK_SENT) {
+                throw ValidationException::withMessages([
+                    'email' => [__($status)],
+                ]);
+            }
+
+            return response()->message(__($status));
+
+        } catch (\Exception $e) {
+            return $this->handleException($e);
+        }
     }
 }
