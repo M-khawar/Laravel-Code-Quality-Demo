cdd7ae168fa10e858654dba5003d394fe2886ff1
M-khawar <muhammad.khawar@ilsainteractive.com>
2023-07-10 12:29:13 +0500

    [appended] is_onboarding_completed check included in me api all user response

--- app/Helpers/GlobalConstants.php
+++ app/Helpers/GlobalConstants.php
@@ -3,6 +3,7 @@
 define('ONBOARDING_GROUP_ALIAS', 'onboarding');
 define('ADVISOR_SETTING_GROUP', 'adviser_settings');
 define('SCHEDULING_LINK_ATTR', 'scheduling_link');
+define('FB_GROUP_JOINED', 'joined_facebook_group');
 
 define('WISTIA', 'wistia');
 define('VIMEO', 'vimeo');
