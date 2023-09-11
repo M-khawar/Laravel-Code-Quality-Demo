7fd9e2c9a81613a9f19052b2a3f357e82ff4e263
M-khawar <muhammad.khawar@ilsainteractive.com>
2023-09-11 18:57:35 +0500

    [feature/account-setting][stripe] can_now interface added for testing purpose

--- app/Packages/StripeWrapper/SubscriptionActions/CancelSubscriptionAction.php
+++ app/Packages/StripeWrapper/SubscriptionActions/CancelSubscriptionAction.php
@@ -20,6 +20,11 @@ class CancelSubscriptionAction extends StripeSubscriptionAbstract
             throw new Exception("You had already unsubscribed from this subscription, you are currently on grace period");
         }
 
+        $cancelNow = request()->query('cancel_now');
+        if ($cancelNow && app()->environment('local', 'staging')) {
+            return $user->subscription($this->subscription_name)->cancelNow();
+        }
+
         return $user->subscription($this->subscription_name)->cancel();
     }
 }
