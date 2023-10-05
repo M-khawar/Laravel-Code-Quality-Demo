ff5e6b4fb391209a27d91b1e3ecad2f959da7d47
M-khawar <muhammad.khawar@ilsainteractive.com>
2023-10-05 18:33:58 +0500

    [feature/scheduled-tasks] live_call video fetched

--- app/Http/Controllers/VideoController.php
+++ app/Http/Controllers/VideoController.php
@@ -2,8 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\SettingResource;
 use App\Http\Resources\VideoResource;
-use App\Models\Video;
+use App\Models\{Setting, Video};
 use Illuminate\Http\Request;
 
 class VideoController extends Controller
@@ -15,6 +16,19 @@ class VideoController extends Controller
             $data = new VideoResource($video);
 
             return response()->success(__('messages.video.fetched_successfully'), $data);
+
+        } catch (\Exception $exception) {
+            return $this->handleException($exception);
+        }
+    }
+
+    public function getLiveCall()
+    {
+        try {
+            $adminSettings = Setting::settingFilters(ADMIN_SETTINGS_GROUP)->get();
+            $adminSettings = new SettingResource($adminSettings);
+
+            return response()->success(__('messages.admin_settings.fetched'), $adminSettings);
 
         } catch (\Exception $exception) {
             return $this->handleException($exception);
