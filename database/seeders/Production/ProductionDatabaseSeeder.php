4da77b6b62d8b62ff15e194e2aaae12552aff813
Ahtisham ulhaq <ahtisham.ullhaq@ilsainteractive.com>
2023-12-03 00:27:38 +0500

    agian

--- database/seeders/Production/ProductionDatabaseSeeder.php
+++ database/seeders/Production/ProductionDatabaseSeeder.php
@@ -10,16 +10,16 @@ class ProductionDatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(UserSeeder::class);
-        $this->call(FunnelSeeder::class);
-        $this->call(CourseSeeder::class);
-        $this->call(CompleteCourseSeeder::class);
-        $this->call(CalendarSeeder::class);
-        $this->call(QuestionAndAnswersSeeder::class);
-        $this->call(NotesSeeder::class);
-        $this->call(LeadsSeeder::class);
-        $this->call(AdvisorSettingsSeeder::class);
-        $this->call(SubscriptionSeeder::class);
-        $this->call(PageViewsSeeder::class);
+        // $this->call(FunnelSeeder::class);
+        // $this->call(CourseSeeder::class);
+        // $this->call(CompleteCourseSeeder::class);
+        // $this->call(CalendarSeeder::class);
+        // $this->call(QuestionAndAnswersSeeder::class);
+        // $this->call(NotesSeeder::class);
+        // $this->call(LeadsSeeder::class);
+        // $this->call(AdvisorSettingsSeeder::class);
+        // $this->call(SubscriptionSeeder::class);
+        // $this->call(PageViewsSeeder::class);
         
     }
 }
